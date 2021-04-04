7ff7c65cf3efc334f4f0fbdd12aedb5e8e875a36
divubhuva <divubhva@gmail.com>
2021-04-04 14:31:13 -0700

    Save work

--- src/application/MainScreenController.java
+++ src/application/MainScreenController.java
@@ -6,8 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
